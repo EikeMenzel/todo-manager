cd7994e87b7d47587848b91522829879db048d9c
Tim S <racer4308@gmail.com>
2024-02-28 17:13:24 +0100

    Add lsit todos

---
+++ app/todo-manager-frontend/src/app/services/task/task.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {of} from "rxjs";
+import {Category} from "../../models/Category";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  public getTasks(categoryId: number) {
+    return this.http.get(`/api/v1/categories/${categoryId}`, {observe: 'response'})
+  }
+
+  public getCategories()
+  {
+    const cat1: Category = {
+      id: 1,
+      name: "Küche"
+    }
+    const cat2: Category = {
+      id: 2,
+      name: "Bad"
+    }
+    const cat3: Category = {
+      id: 2,
+      name: "Ne Kategorie welche absoult lang"
+    }
+    return of([
+      cat1,
+      cat2,
+      cat3
+    ])
+    // return this.http.get(`/api/v1/categories`, {observe: 'response'})
+  }
+}
--- app/todo-manager-frontend/src/app/task-list/task-list.component.ts
+++ app/todo-manager-frontend/src/app/task-list/task-list.component.ts
@@ -1,36 +1,65 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {Task} from "../models/Task";
 import {TaskStatus} from "../models/TaskStatus";
 import {Category} from "../models/Category";
 import {Priority} from "../models/Prio";
+import {TaskService} from "../services/task/task.service";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    FaIconComponent,
+    FormsModule
   ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
-export class TaskListComponent {
-  title = 'Todos';
-  todoList: Task[] = [
-    new Task(1, "Toilette putzen", Priority.Low, TaskStatus.NOT_STARTED),
-    new Task(2, "Zimmer aufräumen", Priority.Low, TaskStatus.IN_PROGRESS),
-    new Task(3, "Bett machen", Priority.Low, TaskStatus.DONE),
-    new Task(4, "Add a new feature that is definitely not totally garbage and is just another excuse to avoid the big elephant in the room", Priority.Low, TaskStatus.DONE),
-  ];
+export class TaskListComponent implements OnInit {
+  private taskList: Task[] = [];
+  protected taskListFiltered: Task[] = [];
+  private categoryList: Category[] = []
+  protected categoryListFiltered: Category[] = []
+  protected readonly faSearch = faSearch;
+  protected searchCategoryTerm: string = "";
+  searchTaskTerm: string = "";
 
-  categoryList: Category[] = [
-    {
-      id: 0,
-      name: "Küche"
-    },
-    {
-      id: 1,
-      name: "Bad"
-    }
-  ]
+  onCategorySearchChange() {
+    this.categoryListFiltered = this.categoryList.filter(value => value.name.toLowerCase().includes(this.searchCategoryTerm.toLowerCase()))
+  }
+  onTaskSearchChange() {
+    this.taskListFiltered = this.taskList.filter(value => value.description.toLowerCase().includes(this.searchTaskTerm.toLowerCase()))
+  }
+
+  constructor(
+    private taskService: TaskService
+  ) {
+  }
+
+  ngOnInit() {
+    this.taskList = [
+      new Task(1, "Toilette putzen", Priority.Low, TaskStatus.NOT_STARTED),
+      new Task(2, "Zimmer aufräumen", Priority.Low, TaskStatus.IN_PROGRESS),
+      new Task(3, "Bett machen", Priority.Low, TaskStatus.DONE),
+      new Task(4, "Add a new feature that is definitely not totally garbage and is just another excuse to avoid the big elephant in the room", Priority.Low, TaskStatus.DONE),
+    ];
+    this.taskListFiltered = this.taskList;
+    this.taskService.getCategories().subscribe({
+      next: value => {
+        this.categoryList = value
+        this.categoryListFiltered = value
+      },
+      error: err => {
+      }
+    })
+  }
+
+  selectCategory(id: number) {
+
+  }
 }
